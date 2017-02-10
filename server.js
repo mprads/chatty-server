3e5abddedc021c7cbdf5d7322eaee82fbd4b3e80
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-10 23:02:55 +0000

    fixed indents

--- server.js
+++ server.js
@@ -2,17 +2,12 @@ const express = require('express');
 const WebSocket = require('ws');
 const SocketServer = require('ws').Server;
 const uuid = require('node-uuid');
-
-// Set the port to 4000
 const PORT = 4000;
-
-// Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
+
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-// Create the WebSockets server
 const wss = new SocketServer({ server });
 
   wss.broadcast = function broadcast(data) {
@@ -43,22 +38,21 @@ const wss = new SocketServer({ server });
     ws.on('message', function incoming(messObj) {
       let input = JSON.parse(messObj);
        switch(input.type) {
-      case "postMessage":
-        input.type = "incomingMessage";
-        input.id = uuid.v4();
-       wss.broadcast(JSON.stringify(input));
-        break;
-      case "postNotification":
-        input.type = "incomingNotification";
-        input.id = uuid.v4();
-        wss.broadcast(JSON.stringify(input));
-        break;
-      default:
-        throw new Error("Error");
-    }
+        case "postMessage":
+          input.type = "incomingMessage";
+          input.id = uuid.v4();
+          wss.broadcast(JSON.stringify(input));
+          break;
+        case "postNotification":
+          input.type = "incomingNotification";
+          input.id = uuid.v4();
+          wss.broadcast(JSON.stringify(input));
+          break;
+        default:
+          throw new Error("Error");
+      }
     });
 
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => {
     console.log('Client disconnected');
     wss.broadcast(JSON.stringify({
